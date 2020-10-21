cac0217cb3820c7d5d6925e93908ba5fb06c6721
Jesus Shankar <jshankar@epiqsystems.com>
2020-10-20 17:21:17 -0700

    Added methods for Recipient List & Document List for envelopes

---
+++ ECAR.DocuSign/Models/EnvelopeDocumentModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECAR.DocuSign.Models
+{
+    /// <summary>
+    /// Data model for documents retrieved from DocuSign.
+    /// </summary>
+    public class EnvelopeDocumentModel
+    {
+        /// <summary>
+        /// Envelope ID returned from the DocuSign ceremony.
+        /// </summary>
+        public string EnvelopeId { get; internal set; }
+
+        /// <summary>
+        /// Document ID from DocuSign (integer ID for actual documents, 'certificate' for certificates).
+        /// </summary>
+        public string DocumentId { get; internal set; }
+
+        /// <summary>
+        /// Document GUID used by DocuSign.
+        /// </summary>
+        public string DSDocumentGUID { get; internal set; }
+
+        /// <summary>
+        /// Signature sequence order of the document (999 for certificates).
+        /// </summary>
+        public string Order { get; internal set; }
+
+        /// <summary>
+        /// Name of the document.
+        /// </summary>
+        public string Name { get; internal set; }
+
+        /// <summary>
+        /// Type of the document ('content' for documents, 'summary' for certificates).
+        /// </summary>
+        public string Type { get; internal set; }
+
+    }
+}
---
+++ ECAR.DocuSign/Models/EnvelopeRecipientModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECAR.DocuSign.Models
+{
+    /// <summary>
+    /// Data model for document recipients retrieved from DocuSign.
+    /// </summary>
+    public class EnvelopeRecipientModel
+    {
+        /// <summary>
+        /// Recipient ID (friendly) returned from DocuSign.
+        /// </summary>
+        public string RecipientId { get; set; }
+        
+        /// <summary>
+        /// Client specific Id for the recipient (empty for emailed DocuSign documents).
+        /// </summary>
+        public string ClientUserId { get; set; }
+        
+        /// <summary>
+        /// Date the recipient declined to sign (if available).
+        /// </summary>
+        public string DeclinedDateTime  { get; set; }
+
+        /// <summary>
+        /// Reason the recipient declined to sign (if available).
+        /// </summary>
+        public string DeclinedReason { get; set; }
+        
+        /// <summary>
+        /// Date the envelope was delivered to this recipient.
+        /// </summary>
+        public string DeliveredDateTime { get; set; }
+
+        /// <summary>
+        /// Email address for the recipient.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Name of the recipient.
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Status of the DocuSign envelope for this recipient.
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// The recipient type for this recipient (signer/witness etc.).
+        /// </summary>
+        public string RecipientType { get; set; }
+
+        /// <summary>
+        /// The role name for this recipient.
+        /// </summary>
+        public string RoleName { get; set; }
+        
+        /// <summary>
+        /// Name provided by the recipient to DocuSign at signature ceremony.
+        /// </summary>
+        public string SignatureName { get; set; }
+        
+        /// <summary>
+        /// Date and time the recipient completed the DocuSign ceremony (if available).
+        /// </summary>
+        public string SignedDateTime { get; set; }
+
+        /// <summary>
+        /// Recipient GUID used by DocuSign.
+        /// </summary>
+        public string DSUserGUID { get; set; }
+    }
+}
